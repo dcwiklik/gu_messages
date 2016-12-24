d31216b5b9934d13a7037d915038568ed0988932
Dominik Cwiklik <dcwiklik@gmail.com>
2016-12-24 11:40:50 +0100

    add symfony/config component

--- web/index.php
+++ web/index.php
@@ -1,2 +1,19 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+
+$route = new Route('/foo', array('controller' => 'MyController'));
+$routes = new RouteCollection();
+$routes->add('route_name', $route);
+
+$context = new RequestContext('/');
+
+$matcher = new UrlMatcher($routes, $context);
+
+$parameters = $matcher->match('/foo');
+// array('controller' => 'MyController', '_route' => 'route_name')
